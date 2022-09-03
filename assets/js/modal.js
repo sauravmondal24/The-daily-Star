86bb8b75eb7cfd101419ea2954033384220379ce
sauravmondal24 <sauravmondal321@gmail.com>
2022-09-03 18:58:26 +0600

    Responsive done

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -17,59 +17,61 @@ const displayNewsPage = (newsInfos) => {
 
 		modalBody.innerHTML = `
 
-        <img class="img-fluid rounded-start" style="width: 100%; height: 400px" src="${
-					newsInfo.image_url ? newsInfo.image_url : 'Data is not Found'
-				}" alt="">
-        <p class="card-text py-3">${
-					newsInfo.details ? newsInfo.details : 'Data is not Found'
-				}</p>
+            <img class="img-fluid rounded-start" style="width: 100%; height: 450px" src="${
+							newsInfo.image_url ? newsInfo.image_url : 'Data is not Found'
+						}" alt="">
+            <p class="card-text py-3">${
+							newsInfo.details ? newsInfo.details : 'Data is not Found'
+						}</p>
 
-        <div class="d-flex justify-content-between align-items-center">
-        <div class="post-owner">
-            <div class="card border-0" style="max-width: 250px">
-                <div class="row g-0">
-                    <div class="col-md-3 pt-3">
-                        <img
-                            src="${
-															newsInfo.author
-																? newsInfo.author.img
-																: 'Data is not Found'
-														}"
-                            class="img-fluid rounded"
-                            alt="..."
-                        />
-                    </div>
-                    <div class="col-md-9">
-                        <div class="card-body">
-                            <h6 class="card-title">${
-															newsInfo.author
-																? newsInfo.author.name
-																: 'Data is not Found'
-														}</h6>
-                            <p class="card-text">${
-															newsInfo.author
-																? newsInfo.author.published_date
-																: 'Data is not Found'
-														}</p>
+        <div class="row d-flex text-center justify-content-between align-items-center">
+            <div class="col-md-4 post-owner">
+                <div class="card border-0" style="max-width: 250px">
+                    <div class="row g-0">
+                        <div class="col-md-3 pt-3">
+                            <img
+                                src="${
+																	newsInfo.author
+																		? newsInfo.author.img
+																		: 'Data is not Found'
+																}"
+                                class="img-fluid rounded"
+                                alt="..."
+                            />
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h6 class="card-title">${
+																	newsInfo.author
+																		? newsInfo.author.name
+																		: 'Data is not Found'
+																}</h6>
+                                <p class="card-text">${
+																	newsInfo.author
+																		? newsInfo.author.published_date
+																		: 'Data is not Found'
+																}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="2">
-            <h5><i class="fa-solid fa-eye"> </i> ${
-							newsInfo.total_view ? newsInfo.total_view : 'Data is not Found'
-						} <span>M</span></h5>
-        </div>
-        <div class="3">
-            <i class="fa-solid fa-star-half-stroke"></i>
-            <i class="fa-regular fa-star"></i>
-            <i class="fa-regular fa-star"></i>
-            <i class="fa-regular fa-star"></i>
-            <i class="fa-regular fa-star"></i>
-        </div>
-        
-    </div> 
+            <div class="col-md-4">
+                <h5><i class="fa-solid fa-eye"> </i> ${
+									newsInfo.total_view
+										? newsInfo.total_view
+										: 'Data is not Found'
+								} <span>M</span></h5>
+            </div>
+            <div class="col-md-4">
+                <i class="fa-solid fa-star-half-stroke"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+            </div>
+    
+        </div> 
         `;
 
 		modalSection.appendChild(modalDisplay);
--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ let tabDataDisplay = (data) => {
 TabList();
 
 //==================================
-//    News Displaying Section
+//    News Post Section
 // ====================================
 
 let NewsFeed = (category_id) => {
@@ -67,78 +67,86 @@ let NewsFeed = (category_id) => {
 
 		newses.forEach((allInfo) => {
 			let newsDisplay = document.createElement('div');
-			newsDisplay.classList.add('card', 'my-5', 'p-4');
+			newsDisplay.classList.add('card', 'my-5');
 
 			newsDisplay.innerHTML = `
-		 	<div class="row g-0  d-flex align-items-center">
-			<div class="col-md-3">
-				<img
-				src="${allInfo.thumbnail_url ? allInfo.thumbnail_url : 'Data is not Fund'}"
-				class="img-fluid rounded-start"
-				alt="..."
-				style="width: 350px"
-				/>
-			</div>
-			<div class="col-md-9">
-			<div class="card-body">
-				<h3 class="card-title">${
-					allInfo.title ? allInfo.title : 'Data is not Fund'
-				}</h3>
-				<p class="card-text">
-					${allInfo.details ? allInfo.details.slice(1, 300) : 'Data Is not Fund'}...
-				</p>
 
-				<div
-					class="d-flex justify-content-between align-items-center"
-				>
-					<div class="post-owner">
-						<div class="card border-0" style="max-width: 250px">
-							<div class="row g-0">
-								<div class="col-md-3 pt-3">
-									<img
-										src="${allInfo.author ? allInfo.author.img : 'Data is not Fund'}"
-										class="img-fluid rounded"
-										alt="..."
-									/>
-								</div>
-								<div class="col-md-9">
-									<div class="card-body">
-										<h6 class="card-title">${
-											allInfo.author ? allInfo.author.name : 'Data is not Fund'
-										}</h6>
-										<p class="card-text">${
-											allInfo.author
-												? allInfo.author.published_date
-												: 'Data is not Fund'
-										}</p>
+		 	<div class="row g-0  d-flex align-items-center">
+				<div class="col-md-3">
+					<img
+					src="${allInfo.thumbnail_url ? allInfo.thumbnail_url : 'Data is not Found'}"
+					class="img-fluid rounded-start"
+					alt="..."
+					style="width: 350px"
+					/>
+				</div>
+
+				<div class="col-md-9">
+					<div class="card-body">
+						<h3 class="card-title">${
+							allInfo.title ? allInfo.title : 'Data is not Found'
+						}</h3>
+						<p class="card-text">
+							${allInfo.details ? allInfo.details.slice(1, 300) : 'Data Is not Found'}...
+						</p>
+
+
+						<div class="row text-center  g-0 justify-content-between align-items-center">
+							<div class="col-md-5 post-owner">
+								<div class="card border-0" style="max-width: 250px">
+									<div class="row g-0">
+										<div class="col-md-3 pt-3 ">
+											<img
+												id="author-pic"
+												src="${allInfo.author ? allInfo.author.img : 'Data is not Found'}"
+												class="img-fluid rounded" 
+												alt="..." 
+												
+
+											/>
+										</div>
+										<div class="col-md-9">
+											<div class="card-body">
+												<h6 class="card-title">${
+													allInfo.author
+														? allInfo.author.name
+														: 'Data is not Found'
+												}</h6>
+												<p class="card-text">${
+													allInfo.author
+														? allInfo.author.published_date
+														: 'Data is not Found'
+												}</p>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
+
+
+							<div class="col-md-2">
+								<h5><i class="fa-solid fa-eye"> </i> ${
+									allInfo.total_view ? allInfo.total_view : 'Data is Not Found'
+								} <span>M</span></h5>
+							</div>
+							<div class="col-md-3">
+								<i class="fa-solid fa-star-half-stroke"></i>
+								<i class="fa-regular fa-star"></i>
+								<i class="fa-regular fa-star"></i>
+								<i class="fa-regular fa-star"></i>
+								<i class="fa-regular fa-star"></i>
+							</div>
+							<div class="col-md-2">
+								<button onclick="NewsPageDetails('${allInfo._id}')" data-bs-toggle="modal"
+								data-bs-target="#staticBackdrop" class="btn" href="#"
+									><i class="fa-solid fa-arrow-right-long"></i
+								></button>
+								
+							</div>
 						</div>
 					</div>
-					<div class="2">
-						<h5><i class="fa-solid fa-eye"> </i> ${
-							allInfo.total_view ? allInfo.total_view : 'Data is Not Fund'
-						} <span>M</span></h5>
-					</div>
-					<div class="3">
-						<i class="fa-solid fa-star-half-stroke"></i>
-						<i class="fa-regular fa-star"></i>
-						<i class="fa-regular fa-star"></i>
-						<i class="fa-regular fa-star"></i>
-						<i class="fa-regular fa-star"></i>
-					</div>
-					<div class="4">
-						<button onclick="NewsPageDetails('${allInfo._id}')" data-bs-toggle="modal"
-						data-bs-target="#staticBackdrop" class="btn" href="#"
-							><i class="fa-solid fa-arrow-right-long"></i
-						></button>
-						
-					</div>
 				</div>
-			</div>
-		</div>
-	  </div>
+	  		</div>
 		
 		`;
 			NewsCard.appendChild(newsDisplay);
